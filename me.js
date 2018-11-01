6e88325d3ca345c8560a61dc2ea8dada76c47e6f
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-11-02 01:40:07 +0400

    Create me.js

---
+++ me.js
@@ -0,0 +1,121 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "!"
+
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('                          Bot By : ImD3s_x');
+  console.log('                          Bot is Ready <3');  
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('By : ImD3s_x ');
+  console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+  console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');  
+  console.log('bot is online')
+client.user.setStatus("online");
+});
+
+
+ const devs = ['399697177259147275' , '' , ''];
+const adminprefix = "-";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/ImD3s_x");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+  });
+
+  
+  
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+// ?say
+  if (command === "say") {
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+  
+ 
+
+if (command == "ed") {
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor(0x23b2d6)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
+  
+  
+  
+  
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكم') {      
+    msg.channel.send("وعليكـم الســلام ورحمة الله وبركاتـه")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '<@399697177259147275>') {      
+    msg.channel.send("هلا")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '<@399697177259147275>') {      
+    msg.channel.send("انا ``AFK !``")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'برب') {      
+    msg.channel.send("``tyt <3``")
+  }
+});
+
+
+
+  client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+
+
+client.login("Mzk5Njk3MTc3MjU5MTQ3Mjc1.Drom8w.s-kJzG0U57c7uhUlalgtS4UilI8")
